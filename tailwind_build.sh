318cef859b296b91dee9bcb3584bacece26ef82d
XIII-MC <81795405+XIII-MC@users.noreply.github.com>
2026-08-18 21:59:12 +0200

    Added build tailwind once script

---
+++ tailwind_build.sh
@@ -0,0 +1,5 @@
+podman run --rm -it \
+  -v "$(pwd)":/app:z \
+  -w /app \
+  docker.io/library/node:20-alpine \
+  sh -c "npm install tailwindcss @tailwindcss/cli && npx @tailwindcss/cli -i ./css/input.css -o ./css/styles.css"
